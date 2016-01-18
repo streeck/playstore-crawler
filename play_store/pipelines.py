9c6359ce09f7fc21ae8835820fd52ac2db75f052
Streeck <raverblazer@gmail.com>
2016-01-18 13:03:36 -0200

    Add categories pipeline.

--- play_store/pipelines.py
+++ play_store/pipelines.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy import signals
+from scrapy.xlib.pydispatch import dispatcher
+from scrapy.contrib.exporter import JsonItemExporter
 
 
-class PlayStorePipeline(object):
+class CategoryPipeline(object):
+
+    def __init__(self):
+        self.file = open('categories.json', 'wb')
+        dispatcher.connect(self.spider_opened, signals.spider_opened)
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
+
+    def spider_opened(self, spider):
+        self.exporter = JsonItemExporter(self.file)
+        self.exporter.start_exporting()
+
+    def spider_closed(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
     def process_item(self, item, spider):
+        if spider.name == 'categories':
+            self.exporter.export_item(item)
         return item
